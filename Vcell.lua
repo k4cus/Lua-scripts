e54bee47f59ff27246e05f843868036abc6eaa18
k4cus <82265403+k4cus@users.noreply.github.com>
2021-09-10 09:35:02 +0200

    Update Vcell.lua

--- Vcell.lua
+++ Vcell.lua
@@ -1,9 +1,9 @@
 ---- ##########################################################################################################
 ---- #                                                                                                        #
----- # Voltage per cell script by Kacper Nowak			                                              #
+---- # Voltage per cell script by Kacper Nowak			                                                        #
 ---- #                                                                                                        #
----- # Tested with Crossfire BatRX						                              #
----- # Based on scripts found on the Internet		                                                      #
+---- # Tested with Crossfire BatRX						                                                           #
+---- # Based on scripts found on the Internet		                                                           #
 ---- #                                                                                                        #
 ---- #                                                                                                        #
 ---- # Usage:                                                                                                 #
@@ -11,8 +11,9 @@
 ---- # 1. Copy script file to SDCard location : SCRIPTS/MIXES                                                 #
 ---- # 2. Add custom lua script                                                                               #
 ---- # 3. Select voltage sensor input                                                                         #
----- # 4. Select amount of Lipo cells used. 								      #
----- # 5. Create new telemetry sensor with id 0310                   					      #
+---- # 4. Select amount of Lipo cells used. 								                                            #
+---- # 5. Create new telemetry sensor with id 0310 - accurate value 0.00 precision in Volts                   #
+---- # 6. Create new telemetry sensor with id 0311 - rounded down value with 0.0 precision for speach	        #
 ---- #                                                                                                        #
 ---- # License GPLv3: http://www.gnu.org/licenses/gpl-3.0.html                                                #
 ---- #                                                                                                        #
@@ -24,7 +25,7 @@
 ---- # Kacper Nowak / vberry.net 2021                                                                         #
 ---- #                                                                                                        #
 ---- # Version:                                                                                               #
----- # 1.0                                                                                                    #
+---- # 1.1                                                                                                    #
 ---- ##########################################################################################################
 
 
@@ -33,9 +34,13 @@ local inputs     = { {"Instance", VALUE ,0,7,0}, {"Sensor", SOURCE}, {"Cells", V
 
 local function run(Instance,Voltage_Source, Cell_Count)
 
-   Voltage_Cell      = Voltage_Source / Cell_Count 
-    
-  setTelemetryValue(0x0310, 0, 0 + Instance, Voltage_Cell*100, 1, 2, "VLT"..Instance)
+  Voltage_Cell      = Voltage_Source / Cell_Count
+  Voltage_Cell_RoundDown =  math.floor(Voltage_Cell*10) / 10
+
+  if Voltage_Cell > 0 then
+  	setTelemetryValue(0x0310, 0, 0 + Instance, Voltage_Cell*100, 1, 2, "VLT"..Instance)
+  	setTelemetryValue(0x0311, 0, 0 + Instance, Voltage_Cell_RoundDown*10, 1, 1, "VLT"..Instance)
+  end
 
   return 0
 
